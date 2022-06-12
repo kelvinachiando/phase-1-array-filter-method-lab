6033dec356f47b09ca44a7f00db95a5376182335
kelvinachiando <kelvinostin22gmail.com>
2022-06-12 22:49:53 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+const findMatching =(array, names)=>{
+    return array.filter(q=>{
+        return q.toLowerCase()===names.toLowerCase();
+    })
+}
+const fuzzyMatch =(array, names)=>{
+    const namesLength = names.Length;
+    return array.filter (q =>{
+        return q.slice(0, namesLength)=== names;
+    })
+}
+const matchName = (array, query) =>{
+    return array.filter(q =>{
+        return q.name === query;
+    })
+}
